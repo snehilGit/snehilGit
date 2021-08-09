b1f10f311a620bb375a58f645682e9878cdfcb27
LAPTOP-945B3F8L\nilesh <nilesh@parsl.co>
2021-08-09 21:44:15 +0530

    landing page

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
